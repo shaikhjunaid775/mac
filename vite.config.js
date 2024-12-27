209301c7eab66a11121a82311eec0b6aab451daa
Shaikh Juned <shaikhjunaid775@gmail.com>
2024-12-27 15:14:12 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
+  base: "/mac/",
+  ins: [react()],
   server: {
     host: true, // Allows external access
-    port: 5173, // Optional: set the desired port
+    port: 5173 // Optional: set the desired port
   },
   resolve: {
     alias: {
-      '@': '/src',
-    },
-  },
-})
+      "@": "/src"
+    }
+  }
+});
